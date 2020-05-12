f5731d95f1490bcadcccb3aba118eeb73c4a65e4
Andy Tom <andrew.tom@colorado.edu>
2020-05-12 15:25:59 -0600

    Adds functionality for UserRepositoryClass to calculate average step goals of all users and filters user data for one user

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -3,5 +3,14 @@ class UserRepository {
     this.data = data || null;
     this.users = [];
   }
+  filterUsersData(id) {
+    return typeof id === "number" ? this.data.find(userData => userData.id === id) : null;
+  }
+  averageStepGoal() {
+    let sumStepGoal = this.data.reduce((acc, currentValue) => {
+      return ((acc + currentValue.dailyStepGoal))
+    }, 0)
+    return Math.floor(sumStepGoal / this.data.length)
+  }
 }
 module.exports = UserRepository;
--- tests/UserRepository-test.js
+++ tests/UserRepository-test.js
@@ -5,42 +5,62 @@ const expect = chai.expect;
 // const activityData = data.activityData;
 const UserRepository = require('../src/UserRepository');
 let userRepository;
-let activityData;
+let userData;
 
 describe('UserRepo', () => {
   beforeEach( () => {
-    userRepository = new UserRepository(activityData);
-    activityData = [
+    userData = [
       {
-        "userID": 1,
-        "date": "2019/06/15",
-        "numSteps": 3577,
-        "minutesActive": 140,
-        "flightsOfStairs": 16
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
       },
       {
-        "userID": 2,
-        "date": "2019/06/15",
-        "numSteps": 4294,
-        "minutesActive": 138,
-        "flightsOfStairs": 10
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [
+          9,
+          18,
+          24,
+          19
+        ]
       },
       {
-        "userID": 3,
-        "date": "2019/06/15",
-        "numSteps": 7402,
-        "minutesActive": 116,
-        "flightsOfStairs": 33
+        "id": 3,
+        "name": "Herminia Witting",
+        "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+        "email": "Elwin.Tromp@yahoo.com",
+        "strideLength": 4.4,
+        "dailyStepGoal": 5000,
+        "friends": [
+          19,
+          11,
+          42,
+          33
+        ]
       }]
+    userRepository = new UserRepository(userData);
   });
   it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
   it('should take in a parameter to take in user data', () => {
-    expect(userRepository.data).to.eql(activityData)
+    expect(userRepository.data).to.eql(userData)
   });
   it('should return undefined if not passed a parameter', () => {
-    const sadUserRepo= new UserRepository();
+    const sadUserRepo = new UserRepository();
     expect(sadUserRepo.data).to.eql(null);
   });
   it('should only take in an array of objects', () => {
@@ -50,6 +70,41 @@ describe('UserRepo', () => {
   it('should hold user objects', () => {
     expect(userRepository.users).to.eql([]);
   })
+  it('given a user’s ID, should return users data', () => {
+    expect(userRepository.filterUsersData(2)).to.eql({
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [
+          9,
+          18,
+          24,
+          19
+        ]
+      });
+  });
+  it('filterUsersData should return null if not given an argument', () => {
+    expect(userRepository.filterUsersData()).to.eql(null);
+  });
+  it('filterUsersData should return null if passed in anything except a number', () => {
+    expect(userRepository.filterUsersData('Sad')).to.eql(null);
+    expect(userRepository.filterUsersData([])).to.eql(null);
+    expect(userRepository.filterUsersData({})).to.eql(null);
+  });
+  it('should have a method averageStepGoal', () => {
+    expect(userRepository.averageStepGoal).to.be.a('function');
+  });
+  it('averageStepGoal should return the average step goal of all users', () => {
+    const result = 6666;
+    expect(userRepository.averageStepGoal()).to.eql(result);
+  });
+  //input : id
+  //find all data related to user id
+  //output : user data
+  //
   // it('should', () => {});
   // it('should', () => {});
 
