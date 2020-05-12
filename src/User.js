47e75e2cb772aeaf38cdceed6ec3fb50961001ef
jdvolk <justin.d.volk@gmail.com>
2020-05-12 16:00:11 -0600

    add [roperties for user class/ wrote test to check if it is a function and takes in user data/ still working on null if not passed any data test

--- src/User.js
+++ src/User.js
@@ -1,6 +1,21 @@
 class User {
-  constructor() {
-    
+  constructor(userDetails) {
+    this.id = null || userDetails.id ;
+    this.name = null || userDetails.name ;
+    this.address = null || userDetails.address ;
+    this.email = null || userDetails.email ;
+    this.strideLength = null || userDetails.strideLength ;
+    this.dailyStepGoal = null || userDetails.dailyStepGoal ;
+    this.friends = null || userDetails.friends ;
   }
 }
 module.exports = User;
+/*
+- A User represents a single user
+- It should have a parameter to take in a 
+  userData object
+- Each user holds on to the user properties 
+  from the data file
+- Should have a method to:
+- - Return a user’s first name only
+*/
