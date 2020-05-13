c4c5eefa992e0365137411d879ac2ec620425b6f
Andy Tom <andrew.tom@colorado.edu>
2020-05-12 20:10:50 -0600

    Removes comments

--- src/User.js
+++ src/User.js
@@ -1,3 +1,4 @@
+
 class User {
   constructor(userDetails) {
     if(typeof userDetails === 'object'){
@@ -16,13 +17,7 @@ class User {
     return this.name.split(" ")[0]
   }
 }
-module.exports = User;
-/*
-- A User represents a single user
-- It should have a parameter to take in a
-  userData object
-- Each user holds on to the user properties
-  from the data file
-- Should have a method to:
-- - Return a user’s first name only
-*/
+
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}
