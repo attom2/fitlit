e632d8069910811846040da034e9500f63febe52
Andy Tom <andrew.tom@colorado.edu>
2020-05-12 20:12:56 -0600

    Adds functions to make a random user, display their greeting, and display their info on a card

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+const userId = Math.floor(Math.random() * Math.floor(userData.length)) + 1
+const userCard = document.querySelector('.user-card');
+const userGreeting = document.querySelector('.user-greeting');
+
+window.onload = function() {
+  makeUser();
+}
+function makeUser() {
+  const user1 = new User(userData[userId])
+  displayGreeting(user1);
+  displayUserInfo(user1);
+}
+function displayGreeting(user) {
+  userGreeting.innerText = `Hello ${user.returnFirstName()}`;
+}
+function displayUserInfo(userDetails) {
+  userCard.innerText = `
+  ID : ${userDetails.id}
+  Name : ${userDetails.name}
+  Address : ${userDetails.address}
+  Email : ${userDetails.email}
+  Stride Length : ${userDetails.strideLength}
+  Daily Step Goal :${userDetails.dailyStepGoal}
+  Friends :${userDetails.friends}
+  `
+}
+
+//var userRep = new UserRepository(userData);
