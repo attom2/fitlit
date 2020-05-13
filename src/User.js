69d8658f9e12ee3d187379ffbb4264f9d3b4ca98
Andy Tom <andrew.tom@colorado.edu>
2020-05-12 18:30:39 -0600

    Adds functionality to User class for returning first name and checks sad paths for user Details

--- src/User.js
+++ src/User.js
@@ -1,20 +1,27 @@
 class User {
   constructor(userDetails) {
-    this.id = null || userDetails.id ;
-    this.name = null || userDetails.name ;
-    this.address = null || userDetails.address ;
-    this.email = null || userDetails.email ;
-    this.strideLength = null || userDetails.strideLength ;
-    this.dailyStepGoal = null || userDetails.dailyStepGoal ;
-    this.friends = null || userDetails.friends ;
+    if(typeof userDetails === 'object'){
+      this.id = userDetails.id ;
+      this.name = userDetails.name ;
+      this.address = userDetails.address ;
+      this.email = userDetails.email ;
+      this.strideLength = userDetails.strideLength ;
+      this.dailyStepGoal = userDetails.dailyStepGoal ;
+      this.friends = userDetails.friends ;
+    } else {
+    this.id = null;
+    }
+  }
+  returnFirstName() {
+    return this.name.split(" ")[0]
   }
 }
 module.exports = User;
 /*
 - A User represents a single user
-- It should have a parameter to take in a 
+- It should have a parameter to take in a
   userData object
-- Each user holds on to the user properties 
+- Each user holds on to the user properties
   from the data file
 - Should have a method to:
 - - Return a user’s first name only
