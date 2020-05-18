1f288c3b3b9ef68b734a3cb6b314541e4d5efbe1
jdvolk <justin.d.volk@gmail.com>
2020-05-18 13:45:05 -0600

    add method skeleton for finding miles walked on given day and user id

--- src/Activity.js
+++ src/Activity.js
@@ -1,8 +1,20 @@
 class Activity {
-  constructor() {
+  constructor(data) {
+    typeof data === 'object'? this.data = data : this.data = null;
+  }
 
+  returnMilesWalkedOnADate(id, date) {
+    let userInfo = this.data.filter(user => user.id === id && user.date === date);
+    let milesWalked = userInfo.numSteps 
+    
   }
 }
+/*
+For a specific day (specified by a date), return the miles a user 
+has walked based on their number of steps (use their strideLength 
+to help calculate this)
+*/
+
 if(typeof module !== 'undefined') {
   module.exports = Activity;
 }
