5d05a2145f9e1fa394964e310a7b6564d42c4676
Andy Tom <andrew.tom@colorado.edu>
2020-05-20 15:42:01 -0600

    Adds linting fixes

--- src/User.js
+++ src/User.js
@@ -1,16 +1,16 @@
 
 class User {
   constructor(userDetails) {
-    if(typeof userDetails === 'object'){
-      this.id = userDetails.id ;
-      this.name = userDetails.name ;
-      this.address = userDetails.address ;
-      this.email = userDetails.email ;
-      this.strideLength = userDetails.strideLength ;
-      this.dailyStepGoal = userDetails.dailyStepGoal ;
-      this.friends = userDetails.friends ;
+    if (typeof userDetails === 'object') {
+      this.id = userDetails.id;
+      this.name = userDetails.name;
+      this.address = userDetails.address;
+      this.email = userDetails.email;
+      this.strideLength = userDetails.strideLength;
+      this.dailyStepGoal = userDetails.dailyStepGoal;
+      this.friends = userDetails.friends;
     } else {
-    this.id = null;
+      this.id = null;
     }
   }
   returnFirstName() {
